64c9bb76de76cecdcb04494a2d6702f9614e4836
Victoriuos <110671880+M-Orziboyev@users.noreply.github.com>
2022-12-24 11:55:07 +0500

    Create checklist.py

---
+++ checklist.py
@@ -0,0 +1,36 @@
+menyu = ['osh', 'qozonkabob', 'shashlik', 'norin', 'somsa']
+ovqat = input('Nima ovqat yeysiz?>>>')
+
+
+'manti' in menyu #menyuda manti bormi?
+
+#natija: FALSE
+
+'osh' in menyu; #menyuda osh bormi
+
+#Natija: True
+
+
+if ovqat.lower() in menyu:
+    print('Buyurtma qabul qilindi.')
+else:
+    print('Afsuski bizda bunday taom yo\'q')
+
+
+#Natija: Nima ovqat yeysiz?>>> somsa 
+# Buyurtma qabul qilindi.
+
+#Natija 2 : Nima ovqat yeysiz?>>>dimlama
+# Afsuski bizda bunday taom yo'q
+
+'manti' not in menyu # menyuda manti yo'qmi?
+#Natija: True
+
+'osh' not in menyu # menyuda osh yo'qmi?
+#Natija:False
+
+if ovqat.lower() not in menyu:
+    print('Afsuski bizda bunday taom yo\'q')
+else:
+    print('Buyurtma qabul qilindi.')
+
